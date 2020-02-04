2d211f7a9638e010ca2b917a68edcc07b8ec3d02
Zohir Alhammoud <Z.alhammoud@icloud.com>
2020-02-04 15:04:41 +0100

    startet arbeiten  auf About component

--- code/app/user/src/components/about/About.js
+++ code/app/user/src/components/about/About.js
@@ -5,8 +5,12 @@ import './About.css'
  class About extends Component {
     render() {
         return (
-            <div>
-                Hallo von About.js
+            <div className="sep">
+                    <div className="image-hover img-opacity">
+                        <img src="../barcode_transparent.png" className="img-1"/>
+                        <img src="../2.jpg" className="img-2"/>
+                    </div>
+                    <h2>Name</h2>
             </div>
         )
     }
