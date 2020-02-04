923cd278837fdd742bf3694a0e16a4683cf99b90
Holger Zerbe <Holger.Zerbe@web.de>
2020-02-04 15:00:19 +0100

    Startpage erstellt

--- code/app/user/src/components/startPage/StartPage.js
+++ code/app/user/src/components/startPage/StartPage.js
@@ -5,7 +5,10 @@ class StartPage extends Component {
     render() {
         return (
             <div className="startPage">
-                Hallo von Startpage.js
+                <p className="welcome">Willkommen bei </p>
+                <div className="startPageLogo"><img src="../foodata_logo.png" alt="logo" /></div>
+                <p className="welcome">Deiner Lebensmittelampel-App </p>
+                
             </div>
         )
     }
