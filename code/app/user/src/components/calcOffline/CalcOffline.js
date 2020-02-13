de9b6f7301237cdb0889b4ccbea7109c9f125397
Holger Zerbe <Holger.Zerbe@web.de>
2020-02-13 06:09:10 +0100

    Calcoffline Komponente angefangen

--- code/app/user/src/components/calcOffline/CalcOffline.js
+++ code/app/user/src/components/calcOffline/CalcOffline.js
@@ -1,16 +1,43 @@
-import React, { Component } from 'react'
-import './CalcOffline.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {setProductFoundToFalse} from '../../actions';
+import './CalcOffline.css';
 
 
 class CalcOffline extends Component {
+
+    send = () => {
+        console.log("Button pressed")
+    }
+
     render() {
         return (
-            <div>
-             Hallo von CalcOffline.js
+            <div className="calcDiv">
+
+                <form>
+                <p>Bitte wähle eine Produktgruppe:</p>
+                <select name="produktgruppe">Bitte auswählen
+                    <option value="wasser">Wasser</option>
+                    <option value="getraenk">andere Getränke</option>
+                    <option value="kaese">Käse</option>
+                    <option value="fette">Fette / Öle</option>
+                    <option value="Sonstiges">alle anderen Lebensmittel</option>
+                </select>
+                <p>Bitte gib die folgende Werte pro 100 g bzw. 100 ml ein:</p>
+                <input type="text" name="energie" placeholder="Brennwert in KCAL" />
+                <input type="text" name="kohlenhydrate" placeholder="Kohlenhydrate in g" />
+                <input type="text" name="fett" placeholder="Fett in g" />
+                <input type="text" name="gesaettigteFettsaeuren" placeholder="Gesättigte Fettsäuren in g" />
+                <input type="text" name="salz" placeholder="Salz in g" />
+                <input type="text" name="protein" placeholder="Eiweiß in g" />
+                <input type="text" name="Ballaststoffe" placeholder="Ballaststoffe in g" />
+                <input type="text" name="obstGemueseNuesse" placeholder="Anteil Obst/Gemüse/Nüsse in %" />
+                <p><button onClick = {this.send.bind(this)}>Berechnen</button></p>
+                </form>
 
             </div>
         )
     }
 }
 
-export default CalcOffline
+export default connect(null,{setProductFoundToFalse})(CalcOffline);
