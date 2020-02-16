9efb0df6527d64f216e04ffaa158b493ccff7afe
Holger Zerbe <Holger.Zerbe@web.de>
2020-02-16 11:06:40 +0100

    Fehlermeldung wenn server nicht erreichbar bei server.js eingefügt

--- code/app/user/src/actions.js
+++ code/app/user/src/actions.js
@@ -1,46 +1,73 @@
 export const loadProduct = (EAN) => async dispatch => {
-
-    const url = '/product?ean='+EAN
-    const result = await fetch(url);
-    const data = await result.json();
+    try {
+        const url = '/product?ean='+EAN
+        const result = await fetch(url);
+        const data = await result.json();
+        
+        // console.log(result)
+        // console.log(data)
     
-    console.log(result)
-    console.log(data)
-
-    dispatch({
-        type: 'LOAD_PRODUCT',
-        value: data
-    })
+        dispatch({
+            type: 'LOAD_PRODUCT',
+            value: data
+        })
+    }
+    catch(e) {
+        dispatch({
+            type: 'LOAD_PRODUCT',
+            value: {error: 503,
+                    productFound : false}
+        })
+        
+    }
 }
 
 export const searchProduct = (string) => async dispatch => {
+    try {
+        const url = '/search?q='+string
+        const result = await fetch(url);
+        const data = await result.json();
 
-    const url = '/search?q='+string
-    const result = await fetch(url);
-    const data = await result.json();
-    
-    console.log(result)
-    console.log(data)
+        // console.log(result)
+        // console.log(data)
 
-    dispatch({
-        type: 'SEARCH_PRODUCT',
-        value: data
-    })
+        dispatch({
+            type: 'SEARCH_PRODUCT',
+            value: data
+        })
+    }
+    catch(e) {
+        dispatch({
+            type: 'LOAD_PRODUCT',
+            value: {error: 503,
+                    productFound : false}
+        })
+        
+    }
 }
 
 export const searchById = (id) => async dispatch => {
+    try {
+        const url = '/searchId?id='+id
+        const result = await fetch(url);
+        const data = await result.json();
+        
+        // console.log(result)
+        // console.log(data)
 
-    const url = '/searchId?id='+id
-    const result = await fetch(url);
-    const data = await result.json();
-    
-    console.log(result)
-    console.log(data)
-
-    dispatch({
-        type: 'SEARCH_BY_ID',
-        value: data
-    })
+        dispatch({
+            type: 'SEARCH_BY_ID',
+            value: data
+        })
+    }
+    catch(e) {
+        dispatch({
+            type: 'LOAD_PRODUCT',
+            value: {error: 503,
+                    productFound : false}
+        })
+        
+    }
 }
 
 export const setProductFoundToFalse = () => async dispatch =>{
