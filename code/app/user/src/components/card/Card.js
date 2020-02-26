de3d99c0046c0e605d83fd94147b2916627f2a91
Zohir Alhammoud <Z.alhammoud@icloud.com>
2020-02-26 15:03:26 +0100

    ein Target für soziale netwerke Links im Card Komponente hinzugefügt

--- code/app/user/src/components/card/Card.js
+++ code/app/user/src/components/card/Card.js
@@ -16,10 +16,9 @@ function Card (props){
                     <h2>{props.name}</h2>
                     <h4 className="dev_description">{props.description}</h4>
                     <div className="font_icons">
-                        <a className="icon" href={props.github}>          <FontAwesomeIcon icon={faGithub}   size="3x"  color="black" /></a>
-                        <a className="icon" href={props.linkedin}>        <FontAwesomeIcon icon={faLinkedin} size="3x"  color="black" /></a>
-                        <a className="icon" href={"mailto:"+ props.email}><FontAwesomeIcon icon={faAt}       size="3x"  color="black" /></a> 
-                        
+                        <a className="icon" href={props.github} target="_blank">  <FontAwesomeIcon icon={faGithub}   size="3x"  color="black" /></a>
+                        <a className="icon" href={props.linkedin} target="_blank"><FontAwesomeIcon icon={faLinkedin} size="3x"  color="black" /></a>
+                        <a className="icon" href={"mailto:"+ props.email}>        <FontAwesomeIcon icon={faAt}       size="3x"  color="black" /></a>
                     </div>
             </div>
         )
